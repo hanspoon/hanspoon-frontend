da942129e88d485a90a6485ac405e1dd80770a88
anonymousRecords <a.Records2022@gmail.com>
2025-12-23 15:03:53 +0900

    chore: tanstack query & devtools 설치

--- apps/viewer/src/main.tsx
+++ apps/viewer/src/main.tsx
@@ -1,22 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import AuthCallback from "./components/AuthCallback.tsx";
 import Share from "./pages/SharePage.tsx";
 
+const queryClient = new QueryClient();
+
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<App />} />
-					<Route path="/auth/callback" element={<AuthCallback />} />
-					<Route path="/share/:shareId" element={<Share />} />
-				</Routes>
-			</BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<App />} />
+						<Route path="/auth/callback" element={<AuthCallback />} />
+						<Route path="/share/:shareId" element={<Share />} />
+					</Routes>
+				</BrowserRouter>
+				<ReactQueryDevtools initialIsOpen={true} />
+			</QueryClientProvider>
 		</StrictMode>,
 	);
 }
