aecf5951b4e78be7f9ceea3a59a53f10c8eb83d4
anonymousRecords <a.Records2022@gmail.com>
2025-12-29 15:20:39 +0900

    feat: 리사이즈 & 드래그 가능한 포스트 그리드 컴포넌트 구현

--- apps/viewer/src/components/share/PostGrid.tsx
+++ apps/viewer/src/components/share/PostGrid.tsx
@@ -1,8 +1,23 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import ResponsiveReactGridLayout, {
+	type ResizeHandleAxis,
+	useContainerWidth,
+} from "react-grid-layout";
 import { allPostQueryOptions } from "../../queries/post";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import { useNavigate } from "react-router";
 
-export const PostGrid = () => {
+const HANDLES: ResizeHandleAxis[] = ["s", "w", "e", "n"];
+
+interface PostGridProps {
+	isEditable: boolean;
+}
+
+export const PostGrid = ({ isEditable }: PostGridProps) => {
+	const { width, containerRef, mounted } = useContainerWidth();
 	const { data: posts } = useSuspenseQuery(allPostQueryOptions());
+	const navigate = useNavigate();
 
 	return (
 		<div
@@ -12,19 +27,44 @@ export const PostGrid = () => {
 				gap: "12px",
 			}}
 		>
-			{posts.map((post) => (
-				<div
-					key={post.id}
-					style={{
-						border: "1px solid #EBEBEB",
-						aspectRatio: "1/1",
-						borderRadius: "24px",
-						padding: "24px",
-					}}
-				>
-					{post.title}
-				</div>
-			))}
+			<div ref={containerRef} style={{ width: "820px" }}>
+				{mounted && (
+					<ResponsiveReactGridLayout
+						layout={posts.map((post, index) => ({
+							i: post.id,
+							x: index % 3,
+							y: Math.floor(index / 3),
+							w: 1,
+							h: 1.5,
+							resizeHandles: isEditable ? HANDLES : [],
+							isDraggable: isEditable,
+							isResizable: isEditable,
+						}))}
+						width={width}
+						gridConfig={{ cols: 3, rowHeight: 180 }}
+					>
+						{posts.map((post) => {
+							return (
+								<button
+									onClick={() =>
+										!isEditable && navigate(`/share/${post.share_id}`)
+									}
+									type="button"
+									key={post.id}
+									style={{
+										border: "1px solid #EBEBEB",
+										aspectRatio: "1/1",
+										borderRadius: "24px",
+										padding: "24px",
+									}}
+								>
+									{post.title}
+								</button>
+							);
+						})}
+					</ResponsiveReactGridLayout>
+				)}
+			</div>
 		</div>
 	);
 };
