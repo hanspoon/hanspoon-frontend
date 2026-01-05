9fbc0647f4356934b911639f4b8d4d905ed69ead
anonymousRecords <a.Records2022@gmail.com>
2026-01-05 12:38:32 +0900

    feat: 미디엄 초기 테스트코드 작성

--- apps/extension/tests/specs/medium-higlight.spec.ts
+++ apps/extension/tests/specs/medium-higlight.spec.ts
@@ -1,17 +1,19 @@
 import { expect, test } from "../fixtures/fixtures";
 import { getSelectedText, selectTextRe } from "../pages/base";
 
-test.describe("벨로그 요소별 하이라이트 상세 테스트", () => {
+test.describe("미디엄 요소별 하이라이트 상세 테스트", () => {
 	test("일반 텍스트 및 인용문 하이라이트 영역 선택 검증", async ({ page }) => {
 		await page.goto(
-			"https://velog.io/@hanspoon/%ED%85%8C%EC%8A%A4%ED%8A%B8-%ED%95%98%EC%9D%B4%EB%9D%BC%EC%9D%B4%ED%8A%B8-%EC%9D%B5%EC%8A%A4%ED%85%90%EC%85%98-%EA%B8%B0%EB%8A%A5-%EA%B2%80%EC%A6%9D%EC%9A%A9-%EC%83%98%ED%94%8C-%EC%95%84%ED%8B%B0%ED%81%B4",
+			"https://medium.com/@gachon2020/test-highlight-extension-verification-for-medium-e43e8b56cdbe",
 		);
-		const text = "이 부분은 인용문(Blockquote)입니다";
-		const regex = /이 부분은 인용문\(Blockquote\)입니다/u;
+		const text =
+			"This post is designed to test text selection, image exclusion, code block handling, and UI rendering of the browser extension.";
+		const regex =
+			/This post is designed to test text selection, image exclusion, code block handling, and UI rendering of the browser extension./u;
 
-		const velogContentLocator = page.locator(".atom-one");
+		const mediumContentLocator = page.locator("article");
 
-		await selectTextRe(velogContentLocator.getByText(text), regex);
+		await selectTextRe(mediumContentLocator.getByText(text), regex);
 
 		await page.mouse.up();
 
