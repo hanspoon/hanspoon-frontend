6a2852d2edeee075cb665b9be9e70d317a989868
anonymousRecords <a.Records2022@gmail.com>
2026-01-13 17:42:44 +0900

    fix: 메모리 용량 에러에 따른 MaxListeners 지정

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -1,6 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
+process.setMaxListeners(50);
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	modules: ["@wxt-dev/module-react"],
