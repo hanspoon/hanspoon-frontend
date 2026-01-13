d1171cec08000c1953fb9b2899f5f7a7fe3c521d
anonymousRecords <a.Records2022@gmail.com>
2026-01-13 20:06:14 +0900

    chore: 팝업 임시로 구현한 로그인 섹션 제거

--- apps/extension/entrypoints/popup/App.tsx
+++ apps/extension/entrypoints/popup/App.tsx
@@ -1,11 +1,5 @@
-import UserAccount from "./components/AuthSection";
-
 const App = () => {
-	return (
-		<div style={{ padding: "16px", width: "300px" }}>
-			<UserAccount />
-		</div>
-	);
+	return <div style={{ padding: "16px", width: "300px" }}>한스푼</div>;
 };
 
 export default App;
