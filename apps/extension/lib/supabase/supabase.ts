b0ed1ec2b0b23a6848c08ed18ebc474a3bdac4d7
anonymousRecords <a.Records2022@gmail.com>
2026-02-02 14:16:53 +0900

    chore: supabase 데이터 타입 추론

--- apps/extension/lib/supabase/supabase.ts
+++ apps/extension/lib/supabase/supabase.ts
@@ -1,7 +1,8 @@
 import type { Session } from "@supabase/supabase-js";
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../../types/database.types";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
 	import.meta.env.VITE_SUPABASE_URL,
 	import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
